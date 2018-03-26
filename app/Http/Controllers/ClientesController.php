ebcc6333d61c20efaea95a79381661ff2e3d5fd0
Julio Cesar Veras <juliocesarveras@Julios-MacBook-Pro.local>
2018-03-25 21:56:26 -0400

    subir cambios en Ocean Digital

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -22,11 +22,6 @@ class ClientesController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    
     public function index()
     {
         $clientes= Cliente::all();
